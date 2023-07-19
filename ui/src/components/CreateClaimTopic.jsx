341fe167ad93c87dc96d7de0864909c9322db145
divyanshjain9 <divyansh.b@rejolut.com>
2023-07-19 13:14:09 +0530

    fix : paddings

--- ui/src/components/CreateClaimTopic.jsx
+++ ui/src/components/CreateClaimTopic.jsx
@@ -8,10 +8,10 @@ function CreateClaimTopic() {
         <div >
             <p className='text-xl p-6'>Create Claim Topic</p>
             <hr></hr>
-            <div className='p-6 mt-2'>
+            <div className='p-3 mt-2'>
                 <div>
                     <p>Claim Topic display name *</p>
-                    <div className='mt-3 relative w-full flex border rounded-lg'>
+                    <div className='mt-3 ml-1 relative w-full flex border rounded-lg p-0'>
                         <Input value={displayName} className='border w-full p-2 rounded-lg text-xl' placeholder='Base KYC' type='text' maxLength={32} onChange={(e) => setDisplayName(e.target.value)} />
                         <p className='absolute right-5 top-2'>{displayName.length}/32</p>
                     </div>
@@ -19,7 +19,7 @@ function CreateClaimTopic() {
                 </div>
                 <div className='mt-10 mb-6'>
                     <p>TopicID hidden name *</p>
-                    <div className='mt-3 relative w-full flex border rounded-lg'>
+                    <div className='mt-3 ml-1 relative w-full flex border rounded-lg p-0'>
                         <Input value={hiddenName} className='border w-full p-2 rounded-lg text-xl' placeholder='1' type='text' maxLength={32} disabled style={{ backgroundColor: "white", cursor: "default" }} />
                         <p className='absolute right-5 top-2'>{hiddenName.length}/32</p>
                     </div>
