f2d5742af2e47db531a52106c4542b78a6e6e29d
divyanshjain9 <divyansh.b@rejolut.com>
2023-07-19 15:48:15 +0530

    fix : file Rename for upper casing

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -15,7 +15,7 @@ import IdentitiesPage from './components/IdentitiesPage.jsx';
 import ClaimsPage from './components/ClaimsPage.jsx';
 
 import BlockchainService from './services/BlockchainService.js';
-import Login from './components/login.jsx';
+import Login from './components/Login.jsx';
 
 function UnsupportedNetworkDialog(props: any) {
 
